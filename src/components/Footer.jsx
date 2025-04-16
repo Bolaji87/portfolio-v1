7b9b5076c01bc71df441b33445164d2d188493bd
Bolaji87 <sakabolaji77@gmail.com>
2025-04-16 13:41:20 +0100

    feat: create Footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="py-4 text-center text-sm font-semibold">
+      &copy; {new Date().getFullYear()} Saka Bolaji Waheed. All rights reserved
+    </footer>
+  );
+}
+
+export default Footer;
