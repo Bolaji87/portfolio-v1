b3d21253dbf48622ae9a954401aa7b4128556c78
Bolaji87 <sakabolaji77@gmail.com>
2025-04-18 02:19:16 +0100

    feat: make some changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,13 +5,16 @@ import Button from "../ui/Button";
 
 function Home() {
   return (
-    <div className="flex py-20 w-full px-8  justify-between  items-center">
+    <div className="flex sm:flex-row justify-center flex-col py-20 w-full px-8 sm:px-12 md:px-14 lg:px-24  sm:justify-between items-center">
       <div>
-        <h1 className="font-semibold mb-5 text-6xl text-gray-800 tracking-wide">
-          <span className="">Hi, I'm Bolaji</span> <br />
+        <h1 className="text-center sm:text-left font-semibold mb-5 text-6xl text-gray-800 tracking-wide">
+          <span className="">
+            <span className="text-blue-600"> Saka Bolaji Waheed</span>
+          </span>{" "}
+          <br />
           <span className="">a React.js Developer</span>
         </h1>
-        <div className="my-10 space-x-2">
+        <div className="my-10 space-x-2 flex justify-center sm:justify-normal ">
           <Button href="#contact" type="secondary">
             contact
           </Button>
@@ -19,7 +22,7 @@ function Home() {
             projects
           </Button>
         </div>
-        <div className="flex mt-5 items-center font-semibold gap-4">
+        <div className="flex mt-5 items-center justify-center sm:justify-normal font-semibold gap-4">
           <div className=" p-3 rounded-full bg-blue-200">
             <Github className="bg-slate-50 rounded-full text-blue-600 font-semibold text-xl" />
           </div>
